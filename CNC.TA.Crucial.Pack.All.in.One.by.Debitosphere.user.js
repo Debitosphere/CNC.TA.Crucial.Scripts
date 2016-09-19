23415ec3a4b1b5807e11dc980ffe70361727575d
Debitosphere <debitosphere@gmail.com>
2016-09-20 01:24:53 +1000

    CNC TA Crucial Pack All in One
    CNCopt Fix Forge issue

--- CNC.TA.Crucial.Pack.All.in.One.by.Debitosphere.user.js
+++ CNC.TA.Crucial.Pack.All.in.One.by.Debitosphere.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        CnC TA: Crucial Pack All in One by DebitoSphere
 // @description Contains every crucial script that is fully functional and updated constantly.
-// @version     1.0.45
+// @version     1.0.46
 // @author      DebitoSphere
 // @homepage    https://www.allyourbasesbelong2us.com
 // @namespace   AllYourBasesbelong2UsCrucialPackAllinOne
@@ -20,7 +20,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // This Pack includes all crucial scripts needed to play the game. They are in the correct order to ensure the least amount of script errors.
 // ==/UserScript==
-var CrucialScriptVersion = "1.0.45";
+var CrucialScriptVersion = "1.0.46";
 
 var GM_SuperValue = new function () {
 
@@ -1388,7 +1388,7 @@ End of Multisession login
 							var VerNumb;
 							var ScriptUrl;
 							var DonateUrl = "https://www.allyourbasesbelong2us.com/donate.html";
-							var CrucialScriptVersion = "1.0.45";
+							var CrucialScriptVersion = "1.0.46";
 							function fetchUpdateData(){
 								var xmlhttp = new XMLHttpRequest();
 								var params = "functionname=Updates";                
