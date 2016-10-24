5f484fa05e3656c94895f4592bc153b5a3135033
Debitosphere <debitosphere@gmail.com>
2016-10-25 06:13:21 +1000

    CNC TA Crucial Pack All in One
    Minor Bug fix for World Map Link

--- CNC.TA.Crucial.Pack.All.in.One.by.Debitosphere.user.js
+++ CNC.TA.Crucial.Pack.All.in.One.by.Debitosphere.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        CnC TA: Crucial Pack All in One by DebitoSphere
 // @description Contains every crucial script that is fully functional and updated constantly.
-// @version     1.0.46
+// @version     1.0.47
 // @author      DebitoSphere
 // @homepage    https://www.allyourbasesbelong2us.com
 // @namespace   AllYourBasesbelong2UsCrucialPackAllinOne
@@ -20,7 +20,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // This Pack includes all crucial scripts needed to play the game. They are in the correct order to ensure the least amount of script errors.
 // ==/UserScript==
-var CrucialScriptVersion = "1.0.46";
+var CrucialScriptVersion = "1.0.47";
 
 var GM_SuperValue = new function () {
 
@@ -1388,7 +1388,7 @@ End of Multisession login
 							var VerNumb;
 							var ScriptUrl;
 							var DonateUrl = "https://www.allyourbasesbelong2us.com/donate.html";
-							var CrucialScriptVersion = "1.0.46";
+							var CrucialScriptVersion = "1.0.47";
 							function fetchUpdateData(){
 								var xmlhttp = new XMLHttpRequest();
 								var params = "functionname=Updates";                
@@ -33507,10 +33507,6 @@ Start of CnCTA WorldMap Link
 						});
 
 						mapButton.addListener('execute', this.onClickMap, this);
-						qx.core.Init.getApplication().getReportReplayOverlay().add(mapButton, {
-							right: 150,
-							top: 12
-						});	
 	
 					},
 
